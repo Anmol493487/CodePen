f415769ba1decc37cda579df59b5154f68e28553
Code Sandbox <hello@codesandbox.io>
2023-09-14 05:21:55 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.c7269c44755452eaf9d647bbb69f55f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fff659392ee8576813bb2e9de1449c81",
+    "url": "https://anmol493487.github.io/csb-ml6v98/index.html"
+  },
+  {
+    "revision": "f60a037c9a8621c7760e",
+    "url": "https://anmol493487.github.io/csb-ml6v98/static/css/2.16d56f53.chunk.css"
+  },
+  {
+    "revision": "5ce0306255f8262bf4c8",
+    "url": "https://anmol493487.github.io/csb-ml6v98/static/css/main.7d2118c4.chunk.css"
+  },
+  {
+    "revision": "f60a037c9a8621c7760e",
+    "url": "https://anmol493487.github.io/csb-ml6v98/static/js/2.c8da4f27.chunk.js"
+  },
+  {
+    "revision": "3ce6d3ae3f5d1813807c7c9761a87fb3",
+    "url": "https://anmol493487.github.io/csb-ml6v98/static/js/2.c8da4f27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ce0306255f8262bf4c8",
+    "url": "https://anmol493487.github.io/csb-ml6v98/static/js/main.fce47317.chunk.js"
+  },
+  {
+    "revision": "425dfbb1a10ba40fa923",
+    "url": "https://anmol493487.github.io/csb-ml6v98/static/js/runtime-main.422acf84.js"
+  }
+]);
